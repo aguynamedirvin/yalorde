a3e6fcea1098186e4e449542b62d5fa31c352230
Irvin <aguynamedirvin@gmail.com>
2016-03-24 23:15:44 -0600

    Added language code, cleaned up footer so it won't display HTML comments for each social network

--- dist/site/snippets/footer.php
+++ dist/site/snippets/footer.php
@@ -26,20 +26,25 @@
 
                     ?>
 
-                    <!-- Facebook -->
-                    <?php ecco( $facebook->isNotEmpty(), '<li><a href="' . $facebook->toURL() . '"><i class="fa fa-facebook"></i></a></li>'); ?>
+                    <!-- Social Networks -->
+                    <?php
 
-                    <!-- Twitter -->
-                    <?php ecco( $twitter->isNotEmpty(), '<li><a href="' . $twitter->toURL() . '"><i class="fa fa-twitter"></i></a></li>'); ?>
+                    // Facebook
+                    ecco( $facebook->isNotEmpty(), '<li><a href="' . $facebook->toURL() . '"><i class="fa fa-facebook"></i></a></li>');
 
-                    <!-- Pinterest -->
-                    <?php ecco( $pinterest->isNotEmpty(), '<li><a href="' . $pinterest->toURL() . '"><i class="fa fa-pinterest"></i></a></li>'); ?>
+                    // Twitter
+                    ecco( $twitter->isNotEmpty(), '<li><a href="' . $twitter->toURL() . '"><i class="fa fa-twitter"></i></a></li>');
 
-                    <!-- Instagram -->
-                    <?php ecco( $instagram->isNotEmpty(), '<li><a href="' . $instagram->toURL() . '"><i class="fa fa-instagram"></i></a></li>'); ?>
+                    // Pinterest
+                    ecco( $pinterest->isNotEmpty(), '<li><a href="' . $pinterest->toURL() . '"><i class="fa fa-pinterest"></i></a></li>');
 
-                    <!-- Google Plus -->
-                    <?php ecco( $googleplus->isNotEmpty(), '<li><a href="' . $googleplus->toURL() . '"><i class="fa fa-google-plus"></i></a></li>'); ?>
+                    // Instagram
+                    ecco( $instagram->isNotEmpty(), '<li><a href="' . $instagram->toURL() . '"><i class="fa fa-instagram"></i></a></li>');
+
+                    // Google Plus
+                    ecco( $googleplus->isNotEmpty(), '<li><a href="' . $googleplus->toURL() . '"><i class="fa fa-google-plus"></i></a></li>');
+
+                    ?>
                 </ul>
             </div>
 
