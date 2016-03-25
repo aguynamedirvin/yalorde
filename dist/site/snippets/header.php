107747da8ac7599435de34e745b2304fe9e371df
Irvin <aguynamedirvin@gmail.com>
2016-03-24 23:21:12 -0600

    Updated display title for SEO

--- dist/site/snippets/header.php
+++ dist/site/snippets/header.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-        <title><?= $page->title()->html() ?> – <?= $site->title()->html() ?></title>
+        <title><?= e( $page->isHomePage(), $site->title()->html(), $page->title()->html() . ' – ' . $site->title()->html() ); ?></title>
 
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
