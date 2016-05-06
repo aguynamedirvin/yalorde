b2c397e9b7171f0e48298aefd3fbf1be7056b0cb
Irvin <aguynamedirvin@gmail.com>
2016-05-05 18:06:50 -0600

    Fixed pagination error

--- dist/site/controllers/category.php
+++ dist/site/controllers/category.php
@@ -1,28 +1,27 @@
 <?php
 
 return function($site, $pages, $page) {
-  
+
     $category = $page;
-    
+
     // Fetch the products
     $products = $category->index()->visible()->filterBy('template', 'product');
-    
-    // Get total prodict result count 
+
+    // Get total prodict result count
     $product_count = $products->count();
-    
-    // Add pagination
-    $pagination = $products->paginate(16)->pagination();
-    
-    
+
     // Filter by occasion
     $occasion = get('occasion');
-    
+
     if ( $occasion != '' ) {
         $test = page($category . '/' . strtolower($occasion));
         $products = $test->children()->visible()->filterBy('template', 'product');
     }
-    
-    
+
+    // Add pagination
+    $products = $products->paginate(16);
+    $pagination = $products->pagination();
+
     return compact('category', 'products', 'product_count', 'pagination');
-  
+
 };
